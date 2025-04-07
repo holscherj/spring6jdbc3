b84b24620e5894249c5249b7f38383740447b0fa
holscherj <holsche2@msu.edu>
2025-04-07 17:25:04 -0400

    Mod 4 - Creating records functionality implemented

--- src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.pluralsight.conference.repository;
 
 import com.pluralsight.conference.model.Speaker;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,12 +10,25 @@ import java.util.List;
 @Repository("speakerRepository")
 public class SpeakerRepositoryImpl implements SpeakerRepository {
 
+    private JdbcTemplate jdbcTemplate;
+
+    public SpeakerRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public List<Speaker> findAll() {
         Speaker speaker = new Speaker();
-        speaker.setName("Bryan Hansen");
+        speaker.setName("Jack H");
         speaker.setSkill("Java");
         List<Speaker> speakers = new ArrayList<>();
         speakers.add(speaker);
         return speakers;
+    }
+
+    @Override
+    public Speaker create(Speaker speaker) {
+        jdbcTemplate.update("INSERT INTO speaker (name) VALUES (?)", speaker.getName());
+
+        return null;
     }
 }
