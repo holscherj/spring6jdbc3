89a43de39cc7d664989c921f583ea4ba92d9e3d7
holscherj <holsche2@msu.edu>
2025-04-07 17:47:36 -0400

    Mod 5 - Reading records functionality implemented

--- src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
@@ -1,9 +1,17 @@
 package com.pluralsight.conference.repository;
 
 import com.pluralsight.conference.model.Speaker;
+import com.pluralsight.conference.repository.util.SpeakerRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,18 +25,30 @@ public class SpeakerRepositoryImpl implements SpeakerRepository {
     }
 
     public List<Speaker> findAll() {
-        Speaker speaker = new Speaker();
-        speaker.setName("Jack H");
-        speaker.setSkill("Java");
-        List<Speaker> speakers = new ArrayList<>();
-        speakers.add(speaker);
+        List<Speaker> speakers = jdbcTemplate.query("select * from speaker", new SpeakerRowMapper());
         return speakers;
     }
 
     @Override
     public Speaker create(Speaker speaker) {
-        jdbcTemplate.update("INSERT INTO speaker (name) VALUES (?)", speaker.getName());
+        //jdbcTemplate.update("INSERT INTO speaker (name) VALUES (?)", speaker.getName());
 
-        return null;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement("INSERT INTO speaker (name) VALUES (?)", new String[] {"id"});
+                ps.setString(1, speaker.getName());
+                return ps;
+            }
+        }, keyHolder);
+
+        Number id = keyHolder.getKey();
+
+        return getSpeaker(id.intValue());
+    }
+
+    private Speaker getSpeaker(int id) {
+        return jdbcTemplate.queryForObject("select * from speaker where id = ?", new SpeakerRowMapper(), id);
     }
 }
--- src/test/java/com/pluralsight/conference/Spring6jdbc3ApplicationTests.java
+++ src/test/java/com/pluralsight/conference/Spring6jdbc3ApplicationTests.java
@@ -19,9 +19,11 @@ class Spring6jdbc3ApplicationTests {
         RestTemplate restTemplate = new RestTemplate();
 
         Speaker speaker = new Speaker();
-        speaker.setName("John H");
+        speaker.setName("Jark H");
 
-        restTemplate.put("http://localhost:8080/speaker", speaker);
+        speaker = restTemplate.postForObject("http://localhost:8080/speaker", speaker, Speaker.class);
+
+        System.out.println(speaker.getName());
     }
 
     @Test
